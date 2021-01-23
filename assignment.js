55a723aa408dc36dd4b1639a4f252f96500e2a24
mosaddekhimel19 <himelmh.2021@gmail.com>
2021-01-23 19:26:47 +0600

    third assignment

---
+++ assignment.js
@@ -0,0 +1,26 @@
+
+
+function kilometerToMeter(kilometer){
+    var meter = kilometer*1000;
+    return meter;
+}
+var value = kilometerToMeter(266);
+console.log(value);
+
+function budgetCalculator(watchNumber,phoneNumber,laptopNumber){
+    var watchPrice = 50;
+    var watchTotal = 50 * watchNumber;
+
+    var phonePrice = 100;
+    var phoneTotal = 100 * phoneNumber;
+
+    var laptopPrice = 500;
+    var laptopTotal = 500 * laptopNumber;
+
+    budgetCalculator =(watchNumber+phoneNumber+laptopNumber);
+
+    return total;
+
+}
+var watchTotal = budgetCalculator(4,7,9);
+console.log(watchTotal)
